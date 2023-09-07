7e86249f1f2e42a4982127b5d4d4e69b570e9753
Nomthandazo Ndweni <ndweni2658@gmail.com>
2023-09-07 15:23:06 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnChangetitle = New Button()
+        SuspendLayout()
+        ' 
+        ' btnChangetitle
+        ' 
+        btnChangetitle.Location = New Point(333, 108)
+        btnChangetitle.Name = "btnChangetitle"
+        btnChangetitle.Size = New Size(141, 87)
+        btnChangetitle.TabIndex = 0
+        btnChangetitle.Text = "Change title "
+        btnChangetitle.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnChangetitle)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnChangetitle As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub btnChangetitle_Click(sender As Object, e As EventArgs) Handles btnChangetitle.Click
+
+        Dim stTitleBar As String
+
+        stTitleBar = "Advice from Elder"
+
+        Me.Text = stTitleBar
+
+
+    End Sub
+End Class
